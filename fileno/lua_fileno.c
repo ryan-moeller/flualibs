e503d913e29f4d9f39a30afd4731c11509109bce
Ryan Moeller <ryan-moeller@att.net>
2024-02-07 00:03:58 -0500

    Add missed fileno module

---
+++ fileno/lua_fileno.c
@@ -0,0 +1,61 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024, Ryan Moeller <ryan-moeller@att.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+#include "luaerror.h"
+
+int luaopen_fileno(lua_State *);
+
+static int
+l_fileno(lua_State *L)
+{
+	luaL_Stream *stream;
+	int fd;
+
+	stream = (luaL_Stream *)luaL_checkudata(L, 1, LUA_FILEHANDLE);
+	lua_assert(stream->f);
+	fd = fileno(stream->f);
+	lua_pushinteger(L, fd);
+	return (1);
+}
+
+int
+luaopen_fileno(lua_State *L)
+{
+	luaL_getmetatable(L, LUA_FILEHANDLE);
+	lua_getfield(L, -1, "__index");
+	lua_pushcfunction(L, l_fileno);
+	lua_setfield(L, -2, "fileno");
+	return (0);
+}
