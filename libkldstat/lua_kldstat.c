38906e223d5c36781007fbc48269ed2cdaedec1b
Ryan Moeller <ryan-moeller@att.net>
2025-02-20 01:15:22 -0500

    libkldstat: Simplify and fix correctness

--- libkldstat/lua_kldstat.c
+++ libkldstat/lua_kldstat.c
@@ -49,27 +49,23 @@ l_kldstat_iter_next(lua_State *L)
 
 	nextid = luaL_checkinteger(L, lua_upvalueindex(1));
 	if (nextid == 0) {
-		return 0;
+		return (0);
 	}
 	stat.version = sizeof (stat);
 	if (kldstat(nextid, &stat) == -1) {
 		luaL_error(L, "cannot kldstat %d: %s", nextid, strerror(errno));
 	}
 	nextid = kldnext(nextid);
-	lua_pushnumber(L, nextid);
+	lua_pushinteger(L, nextid);
 	lua_replace(L, lua_upvalueindex(1));
 
 	lua_newtable(L);
-	lua_pushstring(L, "name");
 	lua_pushstring(L, stat.name);
-	lua_settable(L, -3);
-	lua_pushstring(L, "refs");
-	lua_pushnumber(L, stat.refs);
-	lua_settable(L, -3);
-	lua_pushstring(L, "id");
-	lua_pushnumber(L, stat.id);
-	lua_settable(L, -3);
-	lua_pushstring(L, "address");
+	lua_setfield(L, -2, "name");
+	lua_pushinteger(L, stat.refs);
+	lua_setfield(L, -2, "refs");
+	lua_pushinteger(L, stat.id);
+	lua_setfield(L, -2, "id");
 	{
 		char *s = NULL;
 		if (asprintf(&s, "%p", stat.address) == -1) {
@@ -78,23 +74,21 @@ l_kldstat_iter_next(lua_State *L)
 		lua_pushstring(L, s);
 		free(s);
 	}
-	lua_settable(L, -3);
-	lua_pushstring(L, "size");
-	lua_pushnumber(L, stat.size);
-	lua_settable(L, -3);
-	lua_pushstring(L, "pathname");
+	lua_setfield(L, -2, "address");
+	lua_pushinteger(L, stat.size);
+	lua_setfield(L, -2, "size");
 	lua_pushstring(L, stat.pathname);
-	lua_settable(L, -3);
-	return 1;
+	lua_setfield(L, -2, "pathname");
+	return (1);
 }
 
 static int
 l_kldstat(lua_State *L)
 {
 
-	lua_pushnumber(L, kldnext(0));
+	lua_pushinteger(L, kldnext(0));
 	lua_pushcclosure(L, l_kldstat_iter_next, 1);
-	return 1;
+	return (1);
 }
 
 static int
@@ -105,30 +99,26 @@ l_modstat_next(lua_State *L)
 
 	nextid = luaL_checkinteger(L, lua_upvalueindex(1));
 	if (nextid == 0) {
-		return 0;
+		return (0);
 	}
 	stat.version = sizeof (stat);
 	if (modstat(nextid, &stat) == -1) {
 		luaL_error(L, "cannot modstat %d: %s", nextid, strerror(errno));
 	}
 	nextid = modfnext(nextid);
-	lua_pushnumber(L, nextid);
+	lua_pushinteger(L, nextid);
 	lua_replace(L, lua_upvalueindex(1));
 
 	lua_newtable(L);
-	lua_pushstring(L, "name");
 	lua_pushstring(L, stat.name);
-	lua_settable(L, -3);
-	lua_pushstring(L, "refs");
-	lua_pushnumber(L, stat.refs);
-	lua_settable(L, -3);
-	lua_pushstring(L, "id");
-	lua_pushnumber(L, stat.id);
-	lua_settable(L, -3);
-	lua_pushstring(L, "data");
-	lua_pushnumber(L, stat.data.intval);
-	lua_settable(L, -3);
-	return 1;
+	lua_setfield(L, -2, "name");
+	lua_pushinteger(L, stat.refs);
+	lua_setfield(L, -2, "refs");
+	lua_pushinteger(L, stat.id);
+	lua_setfield(L, -2, "id");
+	lua_pushinteger(L, stat.data.intval);
+	lua_setfield(L, -2, "data");
+	return (1);
 }
 
 static int
@@ -137,9 +127,9 @@ l_modstat(lua_State *L)
 	int fileid;
 
 	fileid = luaL_checkinteger(L, 1);
-	lua_pushnumber(L, kldfirstmod(fileid));
+	lua_pushinteger(L, kldfirstmod(fileid));
 	lua_pushcclosure(L, l_modstat_next, 1);
-	return 1;
+	return (1);
 }
 
 static const struct luaL_Reg l_kldstat_funcs[] = {
