26358f68bc063fc19fd7a58a7c7f00c78c95660c
Ryan Moeller <ryan-moeller@att.net>
2025-05-31 14:50:34 -0400

    Remove email address from copyright lines
    I don't plan on keeping this email address.

--- libkldstat/lua_kldstat.c
+++ libkldstat/lua_kldstat.c
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 2023-2025 Ryan Moeller <ryan-moeller@att.net>
+ * Copyright (c) 2023-2025 Ryan Moeller
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
