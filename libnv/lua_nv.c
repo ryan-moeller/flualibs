3fe98ff721f692862ae9ea7db279a2ca3a387999
Ryan Moeller <rmoeller.dev@gmail.com>
2025-12-30 13:44:55 -0500

    nv: Expose metatables and helpers

--- libnv/lua_nv.c
+++ libnv/lua_nv.c
@@ -16,10 +16,9 @@
 #include <lauxlib.h>
 #include <lualib.h>
 
+#include "lua_nv.h"
 #include "utils.h"
 
-#define NVLIST_METATABLE "nvlist_t"
-#define CONST_NVLIST_METATABLE "const nvlist_t"
 #define CNVLIST_METATABLE "nvlist_t cookie"
 
 #define NVLIST_TYPES(X) \
@@ -40,77 +39,6 @@
 	NVLIST_TYPES(X)
 
 int luaopen_nv(lua_State *);
-
-static inline nvlist_t *
-getnvlist(lua_State *L, int idx, const char *metatable)
-{
-	nvlist_t *nvl = NULL;
-
-	if (lua_getmetatable(L, idx)) {
-		luaL_getmetatable(L, metatable);
-		if (lua_rawequal(L, -1, -2)) {
-			nvl = getcookie(L, idx);
-			lua_pop(L, 3);
-		} else {
-			lua_pop(L, 2);
-		}
-	}
-	return (nvl);
-}
-
-static inline nvlist_t *
-checknvlist(lua_State *L, int idx)
-{
-	nvlist_t *nvl;
-
-	nvl = getnvlist(L, idx, NVLIST_METATABLE);
-	luaL_argexpected(L, nvl != NULL, idx, NVLIST_METATABLE);
-	luaL_argcheck(L, nvlist_error(nvl) == 0, idx, "nvlist has error");
-
-	return (nvl);
-}
-
-static inline const nvlist_t *
-checkconstnvlist(lua_State *L, int idx)
-{
-	const nvlist_t *nvl;
-
-	nvl = getnvlist(L, idx, CONST_NVLIST_METATABLE);
-	luaL_argexpected(L, nvl != NULL, idx, CONST_NVLIST_METATABLE);
-	luaL_argcheck(L, nvlist_error(nvl) == 0, idx, "nvlist has error");
-
-	return (nvl);
-}
-
-static inline nvlist_t *
-getanynvlist(lua_State *L, int idx)
-{
-	nvlist_t *nvl = NULL;
-
-	if (lua_getmetatable(L, idx)) {
-		luaL_getmetatable(L, NVLIST_METATABLE);
-		luaL_getmetatable(L, CONST_NVLIST_METATABLE);
-		if (lua_rawequal(L, -1, -2) || lua_rawequal(L, -1, -3)) {
-			nvl = getcookie(L, idx);
-			lua_pop(L, 4);
-		} else {
-			lua_pop(L, 3);
-		}
-	}
-	return (nvl);
-}
-
-static inline nvlist_t *
-checkanynvlist(lua_State *L, int idx)
-{
-	nvlist_t *nvl;
-
-	nvl = getanynvlist(L, idx);
-	luaL_argexpected(L, nvl != NULL, idx, NVLIST_METATABLE);
-	luaL_argcheck(L, nvlist_error(nvl) == 0, idx, "nvlist has error");
-
-	return (nvl);
-}
 
 static inline const char *
 checkname(lua_State *L, int idx, const nvlist_t *nvl, int type)
---
+++ libnv/lua_nv.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2025 Ryan Moeller
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <sys/nv.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+#include "utils.h"
+
+#define NVLIST_METATABLE "nvlist_t"
+#define CONST_NVLIST_METATABLE "const nvlist_t"
+
+static inline nvlist_t *
+getnvlist(lua_State *L, int idx, const char *metatable)
+{
+	nvlist_t *nvl = NULL;
+
+	if (lua_getmetatable(L, idx)) {
+		luaL_getmetatable(L, metatable);
+		if (lua_rawequal(L, -1, -2)) {
+			nvl = getcookie(L, idx);
+			lua_pop(L, 3);
+		} else {
+			lua_pop(L, 2);
+		}
+	}
+	return (nvl);
+}
+
+static inline nvlist_t *
+checknvlist(lua_State *L, int idx)
+{
+	nvlist_t *nvl;
+
+	nvl = getnvlist(L, idx, NVLIST_METATABLE);
+	luaL_argexpected(L, nvl != NULL, idx, NVLIST_METATABLE);
+	luaL_argcheck(L, nvlist_error(nvl) == 0, idx, "nvlist has error");
+
+	return (nvl);
+}
+
+static inline const nvlist_t *
+checkconstnvlist(lua_State *L, int idx)
+{
+	const nvlist_t *nvl;
+
+	nvl = getnvlist(L, idx, CONST_NVLIST_METATABLE);
+	luaL_argexpected(L, nvl != NULL, idx, CONST_NVLIST_METATABLE);
+	luaL_argcheck(L, nvlist_error(nvl) == 0, idx, "nvlist has error");
+
+	return (nvl);
+}
+
+static inline nvlist_t *
+getanynvlist(lua_State *L, int idx)
+{
+	nvlist_t *nvl = NULL;
+
+	if (lua_getmetatable(L, idx)) {
+		luaL_getmetatable(L, NVLIST_METATABLE);
+		luaL_getmetatable(L, CONST_NVLIST_METATABLE);
+		if (lua_rawequal(L, -1, -2) || lua_rawequal(L, -1, -3)) {
+			nvl = getcookie(L, idx);
+			lua_pop(L, 4);
+		} else {
+			lua_pop(L, 3);
+		}
+	}
+	return (nvl);
+}
+
+static inline nvlist_t *
+checkanynvlist(lua_State *L, int idx)
+{
+	nvlist_t *nvl;
+
+	nvl = getanynvlist(L, idx);
+	luaL_argexpected(L, nvl != NULL, idx, NVLIST_METATABLE);
+	luaL_argcheck(L, nvlist_error(nvl) == 0, idx, "nvlist has error");
+
+	return (nvl);
+}
